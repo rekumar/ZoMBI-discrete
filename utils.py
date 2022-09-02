a4fa64fa94dc62121c768a0a0ac999f4e243c5b9
rekumar <rekumar@lbl.gov>
2022-09-02 16:07:55 -0700

    example 3 working discrete on ZT

--- utils.py
+++ utils.py
@@ -1,35 +1,102 @@
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
+from typing import List, Optional
+import diversipy
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
+
+def find_indices_within_bounds(
+    X: np.array, min_vector: np.array, max_vector: np.array
+) -> List[int]:
+    """Return indices for rows of X that are between the min and max vector values
+
+    Args:
+        X (np.array): n x d array of points
+        min_vector (np.array): n points describing minimum allowed value per column d
+        max_vector (np.array): n points describing maximum allowed value per column d
+
+    Returns:
+        List[int]: list of row indices respecting min and max vectors
+    """
+    idx_within_bounds = np.logical_and(
+        (X >= min_vector).all(axis=1), (X <= max_vector).all(axis=1)
+    )
+    return idx_within_bounds
+
 
 def GP_pred(X, GP_model):
-    '''
+    """
     Predict f(X) means and standard deviations from data using GP.
     :param X:           Input dataset, (n,d) array
     :param GP_model:    GP regressor model
     :return:            Predicted posterior means and standard deviations
-    '''
+    """
     mean, std = GP_model.predict(X, return_std=True)
     return mean, std
 
+
 def bounded_LHS(N, min_vector, max_vector):
-    '''
+    """
     Runs bounded Latin Hypercube Sampling (LHS) using the iteratively zoomed-in search space bounds and memory points.
     Used to initialize the start of the forward experiments within each ZoMBI activation.
     :param N:               Number of LHS datapoints to output
     :param min_vector:      A (d,) array of lower bounds
     :param max_vector:      A (d,) array of upper bounds
     :return:                An (N,d) array of X datapoints
-    '''
+    """
 
     samples = []
     for n in range(N):
-    # randomly select points using uniform sampling within limit ranges
-        samples.append(np.random.uniform(low = min_vector, high = max_vector).T)
+        # randomly select points using uniform sampling within limit ranges
+        samples.append(np.random.uniform(low=min_vector, high=max_vector).T)
     return np.vstack(samples)
 
-def progress_bar(n, T, inc, ensemble, text = ''):
-    '''
+
+def discrete_LHS(
+    X: np.array,
+    n: int,
+    return_indices: bool = False,
+    allowed_indices: Optional[List[int]] = None,
+) -> np.array:
+    """Uniformly sample n points from discrete set X.
+
+    Args:
+        X (np.array): set of points to sample from. Each row is a point, columns are features
+        n (int): number of points to sample
+        allowed_indices (Optional[List[int]]): row indices of points in X that can be sampled. If None, assume we can sample from all points
+        return_indices (bool): if True, return the indices of the sampled points. if False, return the sampled points themselves.
+
+    Returns:
+        np.array: points to test. Each row is a point, columns are features. Points are taken from X
+        or
+        np.array: array of booleans corresponding to a rows in X to test
+    """
+    if allowed_indices is None:
+        allowed_indices = list(range(X.shape[0]))
+    else:
+        if min(allowed_indices) < 0 or max(allowed_indices) >= X.shape[0]:
+            raise ValueError("Allowed indices must be between 0 and X.shape[0]!")
+
+    num_possible = len(allowed_indices)
+    if n > num_possible:
+        raise ValueError(
+            f"Cannot sample more points ({n}) than there are in the set ({num_possible})!"
+        )
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(X)
+    boxes = diversipy.psa_partition(X_scaled, n, allowed_indices)
+    indices = np.array([box.obtain_representative_index() for box in boxes])
+    if return_indices:
+        return indices
+    else:
+        return X[indices]
+
+
+def progress_bar(n, T, inc, ensemble, text=""):
+    """
     Progress bar for optimization procedure.
     :param n:           The current experiment number
     :param T:           The total number of experiments to run
@@ -37,26 +104,31 @@ def progress_bar(n, T, inc, ensemble, text = ''):
     :param ensemble:    The current ensemble number
     :param text:        Text to display either standard BO or ZoMBI with activation function name
     :return:            Live updating progress bar
-    '''
+    """
 
-    p = round(n/T, 2)
+    p = round(n / T, 2)
     total = 40
-    step = int(p*total)
-    block = '\u275A'
-    space = '\u205F\u205F'
+    step = int(p * total)
+    block = "\u275A"
+    space = "\u205F\u205F"
     if p >= inc:
-        sys.stdout.write(f"\r{'Ensemble '+str(ensemble + 1)+', '+text+' : ['+block*step + space*(total-step)+'] '}{n + 1} / {T} experiments completed . . .{' '*step}")
+        sys.stdout.write(
+            f"\r{'Ensemble '+str(ensemble + 1)+', '+text+' : ['+block*step + space*(total-step)+'] '}{n + 1} / {T} experiments completed . . .{' '*step}"
+        )
         sys.stdout.flush()
         inc += 0.1
     elif (1 - inc) < 0.1 and inc <= 1:
         step = int(total)
-        sys.stdout.write(f"\r{'Ensemble '+str(ensemble + 1)+', '+text+' : ['+block*step + space*(total-step)+']'} {T} / {T} Complete!{' '*step}")
+        sys.stdout.write(
+            f"\r{'Ensemble '+str(ensemble + 1)+', '+text+' : ['+block*step + space*(total-step)+']'} {T} / {T} Complete!{' '*step}"
+        )
         sys.stdout.flush()
         inc += 0.1
     return inc
 
+
 def plot(fX_min, compute, nregular, bound_l, bound_u, dim, ensemble, activations):
-    '''
+    """
     Plots the results of the ZoMBI optimization procedure, once completed.
     :param fX_min:      An (e,n) array of evaluated fX values, where e is the #ensemble
     :param compute:     An (e,n) array of compute times of the algorithm, where e is the #ensemble
@@ -67,72 +139,143 @@ def plot(fX_min, compute, nregular, bound_l, bound_u, dim, ensemble, activations
     :param ensemble:    The number of model ensemble runs
     :param activations: The number of ZoMBI activations
     :return:            Three plots: running minimum fX, compute time, evolution of bound values
-    '''
+    """
     import matplotlib.pyplot as plt
-    plt.rcParams.update({'font.size': 20})
-    plt.rcParams["axes.linewidth"] = 2.
+
+    plt.rcParams.update({"font.size": 20})
+    plt.rcParams["axes.linewidth"] = 2.0
     plt.rcParams["axes.edgecolor"] = "#505050"
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5), constrained_layout=True)
     if len(fX_min.shape) == 1:
         fX_min = fX_min.reshape(fX_min.shape[0], 1).T
         compute = compute.reshape(compute.shape[0], 1).T
-    fX_min50 = np.percentile(fX_min, q = 50, axis = 0)
-    fX_min10 = np.percentile(fX_min, q = 10, axis = 0)
-    fX_min90 = np.percentile(fX_min, q = 90, axis = 0)
+    fX_min50 = np.percentile(fX_min, q=50, axis=0)
+    fX_min10 = np.percentile(fX_min, q=10, axis=0)
+    fX_min90 = np.percentile(fX_min, q=90, axis=0)
     xs = np.linspace(1, len(fX_min50) + 1, len(fX_min50))
-    ax1.plot(xs[:nregular], fX_min50[:nregular], lw=3, c='orange', marker='o', markersize=5, alpha=0.8, label='Standard')
-    ax1.plot(xs[nregular:], fX_min50[nregular:], lw=3, c='b', marker='o', markersize=5, alpha=0.8, label='ZoMBI')
-    ax1.fill_between(xs[:nregular], fX_min10[:nregular], fX_min90[:nregular], color = 'orange', alpha = 0.2)
-    ax1.fill_between(xs[nregular:], fX_min10[nregular:], fX_min90[nregular:], color = 'b', alpha=0.2)
-    ax1.axvline(nregular, lw=4, c='k', ls=':')
+    ax1.plot(
+        xs[:nregular],
+        fX_min50[:nregular],
+        lw=3,
+        c="orange",
+        marker="o",
+        markersize=5,
+        alpha=0.8,
+        label="Standard",
+    )
+    ax1.plot(
+        xs[nregular:],
+        fX_min50[nregular:],
+        lw=3,
+        c="b",
+        marker="o",
+        markersize=5,
+        alpha=0.8,
+        label="ZoMBI",
+    )
+    ax1.fill_between(
+        xs[:nregular],
+        fX_min10[:nregular],
+        fX_min90[:nregular],
+        color="orange",
+        alpha=0.2,
+    )
+    ax1.fill_between(
+        xs[nregular:], fX_min10[nregular:], fX_min90[nregular:], color="b", alpha=0.2
+    )
+    ax1.axvline(nregular, lw=4, c="k", ls=":")
     lmin = np.min(fX_min)
     lmax = np.max(fX_min)
     xpos = nregular / len(fX_min50)
-    ax1.text(xpos, 1.05, 'Standard \u2190 ', fontsize=20, ha='right', color='orange', transform=ax1.transAxes)
-    ax1.text(xpos, 1.05, ' \u2192 ZoMBI', fontsize=20, ha='left', color='b', transform=ax1.transAxes)
+    ax1.text(
+        xpos,
+        1.05,
+        "Standard \u2190 ",
+        fontsize=20,
+        ha="right",
+        color="orange",
+        transform=ax1.transAxes,
+    )
+    ax1.text(
+        xpos,
+        1.05,
+        " \u2192 ZoMBI",
+        fontsize=20,
+        ha="left",
+        color="b",
+        transform=ax1.transAxes,
+    )
     xtics = [0, nregular, len(fX_min50) + 1]
-    ytics = [lmin, 0., lmax]
+    ytics = [lmin, 0.0, lmax]
     ax1.set_xticks(xtics)
     ax1.set_xticklabels(xtics)
     ax1.set_yticks(ytics)
     ax1.set_yticklabels(np.round(ytics, 2))
-    ax1.set_xlabel('Experiments')
-    ax1.set_ylabel('Value, $f(X)$')
+    ax1.set_xlabel("Experiments")
+    ax1.set_ylabel("Value, $f(X)$")
     ax1.minorticks_on()
-    ax1.grid(which='major', linestyle='-', linewidth='0.5', color='grey', alpha=0.8)
-    ax1.grid(which='minor', linestyle='-', linewidth='0.5', color='grey', alpha=0.8)
-    ax1.axhline(0, lw=1, c='k')
+    ax1.grid(which="major", linestyle="-", linewidth="0.5", color="grey", alpha=0.8)
+    ax1.grid(which="minor", linestyle="-", linewidth="0.5", color="grey", alpha=0.8)
+    ax1.axhline(0, lw=1, c="k")
 
     compute50 = np.percentile(compute, q=50, axis=0)
     compute10 = np.percentile(compute, q=10, axis=0)
     compute90 = np.percentile(compute, q=90, axis=0)
     xs = np.linspace(1, len(compute50) + 1, len(compute50))
-    ax2.plot(xs[:nregular], compute50[:nregular], c='orange', lw=2, marker='o', markersize=5, label='Standard')
-    ax2.plot(xs[:len(compute50[nregular:]+1)], compute50[nregular:], c='b', lw=2, marker='o', markersize=5, label='ZoMBI')
-    ax2.fill_between(xs[:nregular], compute10[:nregular], compute90[:nregular], color = 'orange', alpha = 0.2)
-    ax2.fill_between(xs[:len(compute50[nregular:]+1)], compute10[nregular:], compute90[nregular:], color = 'b', alpha=0.2)
-    ax2.set_yscale('log')
-    ax2.grid(which='major', linestyle='-', linewidth='0.5', color='grey', alpha=0.8)
-    ax2.grid(which='minor', linestyle='-', linewidth='0.5', color='grey', alpha=0.8)
-    ax2.set_ylabel('Compute Time per\nExperiment [s]')
-    ax2.set_xlabel('Experiments')
+    ax2.plot(
+        xs[:nregular],
+        compute50[:nregular],
+        c="orange",
+        lw=2,
+        marker="o",
+        markersize=5,
+        label="Standard",
+    )
+    ax2.plot(
+        xs[: len(compute50[nregular:] + 1)],
+        compute50[nregular:],
+        c="b",
+        lw=2,
+        marker="o",
+        markersize=5,
+        label="ZoMBI",
+    )
+    ax2.fill_between(
+        xs[:nregular],
+        compute10[:nregular],
+        compute90[:nregular],
+        color="orange",
+        alpha=0.2,
+    )
+    ax2.fill_between(
+        xs[: len(compute50[nregular:] + 1)],
+        compute10[nregular:],
+        compute90[nregular:],
+        color="b",
+        alpha=0.2,
+    )
+    ax2.set_yscale("log")
+    ax2.grid(which="major", linestyle="-", linewidth="0.5", color="grey", alpha=0.8)
+    ax2.grid(which="minor", linestyle="-", linewidth="0.5", color="grey", alpha=0.8)
+    ax2.set_ylabel("Compute Time per\nExperiment [s]")
+    ax2.set_xlabel("Experiments")
     ax2.legend(fontsize=12)
     ax1.legend(fontsize=12)
-    plt.show();
+    plt.show()
 
-    plt.figure(figsize=(6,3))
+    plt.figure(figsize=(6, 3))
     for n in range(dim * ensemble):
         if n == 0:
-            plt.plot(bound_l[:, n], c='k', lw=0.5, label = 'Lower Bound')
-            plt.plot(bound_u[:, n], c='r', lw=0.5, label = 'Upper Bound')
+            plt.plot(bound_l[:, n], c="k", lw=0.5, label="Lower Bound")
+            plt.plot(bound_u[:, n], c="r", lw=0.5, label="Upper Bound")
         else:
-            plt.plot(bound_l[:, n], c='k', lw=0.5)
-            plt.plot(bound_u[:, n], c='r', lw=0.5)
+            plt.plot(bound_l[:, n], c="k", lw=0.5)
+            plt.plot(bound_u[:, n], c="r", lw=0.5)
     for a in range(activations):
-        plt.axvline(a, c='k', lw = 1, ls = ':')
-    plt.title('Evolution of Bounds')
-    plt.xlabel('ZoMBI Activation')
-    plt.ylabel('Bound Value')
+        plt.axvline(a, c="k", lw=1, ls=":")
+    plt.title("Evolution of Bounds")
+    plt.xlabel("ZoMBI Activation")
+    plt.ylabel("Bound Value")
     plt.legend(fontsize=12)
-    plt.show();
+    plt.show()
--- zombi.py
+++ zombi.py
@@ -3,22 +3,45 @@
 # import warnings
 # warnings.warn = warn
 import warnings
+
 warnings.filterwarnings("ignore")
 import numpy as np
 import time
 import random
 from skopt.learning.gaussian_process.kernels import ConstantKernel, Matern
-from utils import bounded_LHS, progress_bar, plot
+from utils import discrete_LHS, progress_bar, plot, find_indices_within_bounds
 from acquisitions import *
+from traceback import format_exc
 
-class ZoMBI:
-    def __init__(self, dataset_X, dataset_fX, fX_model, BO, nregular, activations, memory, forward, ensemble,
-                 xi=0.1, beta=1, beta_ab=0.1, eta=0, ratio=3, decay=0.9):
-        '''
+
+class OutOfPointsException(Exception):
+    """ZoMBI bounds have reduced discrete search space too much, no points left to test. forced stop"""
+
+
+class ZoMBI_Discrete:
+    """Modification of the ZoMBI algorithm to work over discrete search spaces
+    """
+    def __init__(
+        self,
+        dataset_X,
+        dataset_fX,
+        BO,
+        nregular,
+        activations,
+        memory,
+        forward,
+        ensemble,
+        xi=0.1,
+        beta=1,
+        beta_ab=0.1,
+        eta=0,
+        ratio=3,
+        decay=0.9,
+    ):
+        """
         Runs the ZoMBI optimization procedure over #activations after #nregular standard BO.
         :param dataset_X:       An (n,d) array of points to run BO on
         :param dataset_fX:      An (n,) array of labels for the respective X values
-        :param fX_model:        Predictive model to obtain f(X) from X
         :param BO:              BO acquisition function: LCB, LCB_ada, EI, EI_abrupt
         :param nregular:        Number of experiments to run regular BO before ZoMBI
         :param activations:     Number of times to activate ZoMBI optimization
@@ -31,24 +54,25 @@ class ZoMBI:
         :param eta:             EI Abrupt learning threshold hyperparameter
         :param ratio:           LCB Adaptive exploration ratio hyperparameter
         :param decay:           LCB Adaptive exponential decay hyperparameter
-        '''
+        """
 
         if not (BO == LCB or BO == EI or BO == LCB_ada or BO == EI_abrupt):
-            raise ValueError("Variable BO must be set to one of the following objects: LCB, EI, LCB_ada, EI_abrupt")
+            raise ValueError(
+                "Variable BO must be set to one of the following objects: LCB, EI, LCB_ada, EI_abrupt"
+            )
         if not (forward > 1):
             raise ValueError("Number of forward experiments must be greater than 1")
 
         # Input parameters
         self.dataset_X = dataset_X
         self.dataset_fX = dataset_fX
-        self.fX_model = fX_model
-        self.BO = BO                    # BO acquisition function to use
-        self.nregular = nregular        # number of regular BO experiments to run before ZoMBI
+        self.BO = BO  # BO acquisition function to use
+        self.nregular = nregular  # number of regular BO experiments to run before ZoMBI
         self.activations = activations  # number of ZoMBI activations
-        self.memory = memory            # number of best memory points to keep
-        self.forward = forward          # number of forward experiments per activation
-        self.ensemble = ensemble        # number of independent ensemble model runs
-        self.batch = 1                  # batch size
+        self.memory = memory  # number of best memory points to keep
+        self.forward = forward  # number of forward experiments per activation
+        self.ensemble = ensemble  # number of independent ensemble model runs
+        self.batch = 1  # batch size
         self.compute = []
 
         # Acquisition function parameters
@@ -60,68 +84,111 @@ class ZoMBI:
         self.decay = decay
 
         # GP model
-        self.GP = GaussianProcessRegressor(kernel = ConstantKernel(1, constant_value_bounds = 'fixed') * Matern(
-            length_scale = 5, length_scale_bounds = 'fixed', nu = 1) * 0.5, n_restarts_optimizer = 30,
-            alpha = 0.0002, normalize_y = False)
+        self.GP = GaussianProcessRegressor(
+            kernel=ConstantKernel(1, constant_value_bounds="fixed")
+            * Matern(length_scale=5, length_scale_bounds="fixed", nu=1)
+            * 0.5,
+            n_restarts_optimizer=30,
+            alpha=0.0002,
+            normalize_y=False,
+        )
 
     def activate_zombi(self, current_act, total_act):
-        '''
+        """
         Activate ZoMBI when called.
         :param current_act:     The current activation number
         :param total_act:       The total number of activations in the procedure
         :return:                Optimized X values, Y values, minimum Y values
-        '''
+        """
 
-        idx_old = np.array([0])
-        mem = np.unique(self.fX_i, return_index=True)[1][-self.memory:]
-        min_vector = np.min(self.X_i[mem, :], axis=0)  # min bounds of last sampled points
-        max_vector = np.max(self.X_i[mem, :], axis=0)  # max bounds of last samples points
+        idx_old = np.zeros((self.batch,))
+        mem = np.unique(self.fX_i, return_index=True)[1][-self.memory :]
+        min_vector = np.min(
+            self.X_i[mem, :], axis=0
+        )  # min bounds of last sampled points
+        max_vector = np.max(
+            self.X_i[mem, :], axis=0
+        )  # max bounds of last samples points
         if current_act == 0:
             self.bound_l_i = min_vector
             self.bound_u_i = max_vector
         else:
             self.bound_l_i = np.vstack([self.bound_l_i, min_vector])
             self.bound_u_i = np.vstack([self.bound_u_i, max_vector])
-        X_bounded = bounded_LHS(N=self.memory, min_vector=min_vector, max_vector=max_vector)  # bounded initialization dataset from LHS
-        fX_bounded = -1 * self.fX_model(X_bounded)
-        bounded_norm = self.X_i.copy()
-        for d in range(bounded_norm.shape[1]):
-            bounded_norm[bounded_norm[:, d] > max_vector[d], d] = max_vector[d]  # upper bound
-            bounded_norm[bounded_norm[:, d] < min_vector[d], d] = min_vector[d]  # lower bound
+
+        # find design points that lie within new bounds
+        idx_within_bounds = find_indices_within_bounds(
+            self.dataset_X, min_vector, max_vector
+        )
+        if len(idx_within_bounds) < (self.memory + self.forward * self.batch):
+            raise OutOfPointsException(
+                f"Not enough points ({len(idx_within_bounds)}) left within bounds ({min_vector} - {max_vector}) to feed the ZoMBI!"
+            )
+            # print("Not enough points left within bounds to feed the ZoMBI!")
+            # return
+        bounded_norm = self.dataset_X[idx_within_bounds]
+        bounded_fX = self.dataset_fX[idx_within_bounds]
+
+        # select initial LHS points from bounded design space
+        idx_lhs = discrete_LHS(X=bounded_norm, n=self.memory, return_indices=True)
+        # idx_lhs_from_total_design_space = idx_within_bounds[idx_lhs]
+        X_bounded = bounded_norm[idx_lhs]
+        fX_bounded = bounded_fX[idx_lhs]
+
         inc = 0.1
         for n in range(self.forward):
-            inc = progress_bar(n = n, T = self.forward, inc = inc, ensemble = self.e,
-                               text = f'ZoMBI Activation {current_act + 1} / {total_act}')
+            inc = progress_bar(
+                n=n,
+                T=self.forward,
+                inc=inc,
+                ensemble=self.e,
+                text=f"ZoMBI Activation {current_act + 1} / {total_act}",
+            )
             X_new = X_bounded
             fX_new = fX_bounded
             start = time.time()
-            self.GP.fit(X_new, fX_new) # fit a zoomed-in, GP resolved near optimum
-            ac_value = self.BO(X=X_new, GP_model=self.GP, n=n, fX_best=self.fX_min_i[-1], fX_best_min=self.fX_min_i,
-                               xi=self.xi, beta=self.beta, beta_ab=self.beta_ab, eta=self.eta,
-                               ratio=self.ratio, decay=self.decay)  # apply zooming constraints to search space
-            self.compute_i.append(time.time()-start)
-            idx = np.argsort(ac_value)[:self.batch]
-            j = 0
-            if idx == idx_old:
-                j = j + 1
-                idx = np.argsort(ac_value)[j + 1:self.batch + j + 1]
+            self.GP.fit(X_new, fX_new)  # fit a zoomed-in, GP resolved near optimum
+            ac_value = self.BO(
+                X=bounded_norm,
+                GP_model=self.GP,
+                n=n,
+                fX_best=self.fX_min_i[-1],
+                fX_best_min=self.fX_min_i,
+                xi=self.xi,
+                beta=self.beta,
+                beta_ab=self.beta_ab,
+                eta=self.eta,
+                ratio=self.ratio,
+                decay=self.decay,
+            )  # apply zooming constraints to search space
+            self.compute_i.append(time.time() - start)
+
+            # Previous implementation could allow duplicate points in new batches. Technically could allow permutations of previous batches as well!
+            # idx = np.argsort(ac_value)[: self.batch]
+            # j = 0
+            # while idx == idx_old:
+            #     j += 1
+            #     idx = np.argsort(ac_value)[j + 1 : self.batch + j + 1]
+            idx = [
+                idx for idx in np.argsort(ac_value) if bounded_norm[idx] not in X_new
+            ][: self.batch]
+
             X_bounded = np.vstack([X_new, bounded_norm[idx]])
             fX_bounded = np.append(fX_new, self.dataset_fX[idx])
-            idx_old = idx
 
         X_bounded_full = np.array(X_bounded)
         fX_bounded_full = np.array(fX_bounded)
         fX_bounded_min = np.array(np.minimum.accumulate(-1 * fX_bounded))
         return X_bounded_full, fX_bounded_full, fX_bounded_min
 
-    def optimize(self, X_initial, fX_initial, plot_f = True):
-        '''
+    def optimize(self, X_initial, fX_initial, plot_f=True):
+        """
         Executes the full BO/ZoMBI optimization procedure when called.
         :param X_initial:       An (n,d) array of initialization points, best to use LHS with n~5
         :param fX_initial:      An (n,) array of evaluated initilization points f(X)
         :param plot_f:          True or False value, plots the function value, compute time, and bounds evolution
         :return:                Call func.X, func.fX to get the optimized
-        '''
+        """
 
         for e in range(self.ensemble):
             self.e = e
@@ -132,10 +199,14 @@ class ZoMBI:
             fX_initial_i = fX_initial
             self.compute_i = []
 
-
-            for n in range(self.nregular): # Regular BO
-                inc = progress_bar(n = n, T = self.nregular, inc = inc, ensemble = self.e,
-                                   text = f'Standard BO ({self.BO.__name__})')
+            for n in range(self.nregular):  # Regular BO
+                inc = progress_bar(
+                    n=n,
+                    T=self.nregular,
+                    inc=inc,
+                    ensemble=self.e,
+                    text=f"Standard BO ({self.BO.__name__})",
+                )
                 X_new = X_initial_i
                 fX_new = fX_initial_i
                 fX_best = max(fX_new)
@@ -143,27 +214,47 @@ class ZoMBI:
                 fX_best_min = np.minimum.accumulate(-1 * np.array(fX_best_list))
                 start = time.time()
                 self.GP.fit(X_new, fX_new)
-                ac_value = self.BO(X=X_new, GP_model=self.GP, n=n, fX_best=fX_best, fX_best_min=fX_best_min,
-                                   xi=self.xi, beta=self.beta, beta_ab=self.beta_ab, eta=self.eta,
-                                   ratio=self.ratio, decay=self.decay)
-                self.compute_i.append(time.time()-start)
-                idx = np.argsort(ac_value)[:self.batch]
+                ac_value = self.BO(
+                    X=X_new,
+                    GP_model=self.GP,
+                    n=n,
+                    fX_best=fX_best,
+                    fX_best_min=fX_best_min,
+                    xi=self.xi,
+                    beta=self.beta,
+                    beta_ab=self.beta_ab,
+                    eta=self.eta,
+                    ratio=self.ratio,
+                    decay=self.decay,
+                )
+                self.compute_i.append(time.time() - start)
+                idx = np.argsort(ac_value)[: self.batch]
                 j = 0
                 if idx == idx_old:
                     j = j + 1
-                    idx = np.argsort(ac_value)[j + 1:self.batch + j + 1]
-                X_initial_i = np.vstack([X_new, self.dataset_X[idx]])  # Grab predicted x-value from dataset
-                fX_initial_i = np.append(fX_new, self.dataset_fX[idx])  # Grab predicted y-value from dataset
+                    idx = np.argsort(ac_value)[j + 1 : self.batch + j + 1]
+                X_initial_i = np.vstack(
+                    [X_new, self.dataset_X[idx]]
+                )  # Grab predicted x-value from dataset
+                fX_initial_i = np.append(
+                    fX_new, self.dataset_fX[idx]
+                )  # Grab predicted y-value from dataset
                 idx_old = idx
             self.X_i = X_initial_i
             self.fX_i = fX_initial_i
             self.fX_min_i = np.minimum.accumulate(-1 * self.fX_i)
 
             for a in range(self.activations):
-                X_i, fX_i, fX_min_i = self.activate_zombi(current_act = a, total_act = self.activations)
-                self.X_i = np.vstack([self.X_i, X_i])
-                self.fX_i = np.hstack([self.fX_i, fX_i])
-                self.fX_min_i = np.hstack([self.fX_min_i, fX_min_i])
+                try:
+                    X_i, fX_i, fX_min_i = self.activate_zombi(
+                        current_act=a, total_act=self.activations
+                    )
+                    self.X_i = np.vstack([self.X_i, X_i])
+                    self.fX_i = np.hstack([self.fX_i, fX_i])
+                    self.fX_min_i = np.hstack([self.fX_min_i, fX_min_i])
+                except OutOfPointsException as e:
+                    print(format_exc(e))
+                    break
             self.fX_min_i = np.minimum.accumulate(self.fX_min_i)
 
             if e == 0:
@@ -182,6 +273,13 @@ class ZoMBI:
                 self.bound_u = np.hstack([self.bound_u, self.bound_u_i])
 
         if plot_f == True:
-            plot(fX_min = self.fX_min, compute = self.compute, nregular = self.nregular,
-             bound_l = self.bound_l, bound_u = self.bound_u, dim = X_i.shape[1], ensemble = self.ensemble, activations = self.activations)
-
+            plot(
+                fX_min=self.fX_min,
+                compute=self.compute,
+                nregular=self.nregular,
+                bound_l=self.bound_l,
+                bound_u=self.bound_u,
+                dim=self.X_i.shape[1],
+                ensemble=self.ensemble,
+                activations=self.activations,
+            )
